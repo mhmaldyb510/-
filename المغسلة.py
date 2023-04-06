79de28e13d879c70934fe8ff10d0b0644164601e
mhmaldyb510 <53155074+mhmaldyb510@users.noreply.github.com>
2023-04-06 21:59:31 +0200

    Add files via upload

---
+++ المغسلة.py
@@ -0,0 +1,8 @@
+print ("اهلا بيك في مغسلة محمد \n")
+car_name = input ("ايه هو نوع عربيتك \n")
+if car_name == "فيات" :
+   print ("اسف مقدرش أساعدك \n")
+else :
+   drink = input ("تحب تشرب ايه \n")
+   print("اتفضل احلى",drink,"\n")
+   print("اتفضل عربيتك")
